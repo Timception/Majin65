1d1721ee6aa830c7b82c95dbca72b46d8e782ac6
Timothy Ryan <84595044+Timception@users.noreply.github.com>
2023-01-30 01:02:56 +0700

    Add files via upload

---
+++ 65/keymaps/default/rules.mk
@@ -0,0 +1,15 @@
+CONSOLE_ENABLE			= no	# Console for debug
+COMMAND_ENABLE			= no	# Commands for debug and configuration
+MOUSEKEY_ENABLE			= yes	# Mouse keys
+VIA_ENABLE				= yes
+VIAL_ENABLE				= yes
+VIAL_INSECURE			= yes
+QMK_SETTINGS			= no
+TAP_DANCE_ENABLE		= no
+COMBO_ENABLE			= no
+KEY_OVERRIDE_ENABLE		= no
+ENCODER_ENABLE			= yes
+ENCODER_MAP_ENABLE		= yes
+VIAL_ENCODERS_ENABLE	= yes
+VIALRGB_ENABLE			= yes
+EXTRAFLAGS += -flto
